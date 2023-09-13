efd6695673096a1de21858ecaa0207ca33263fe8
Kemi1505 <oluwakemikuteyi1505@gmail.com>
2023-09-13 02:36:37 -0700

    101Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistdio.h>
+/**
+ * main - prints standard error
+ * Return: 1(Correct)
+ */
+int main(void)
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
