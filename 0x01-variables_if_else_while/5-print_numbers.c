415155425aa0c62b39e6da4b6f6f89e1161b87f2
Kemi1505 <oluwakemikuteyi1505@gmail.com>
2023-09-24 06:14:51 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints decimal numbers
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+	printf("\n");
+	return (0);
+}
