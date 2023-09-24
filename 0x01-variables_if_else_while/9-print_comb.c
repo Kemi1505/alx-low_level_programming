f27f83c067d6bbcaad392567d1c5a72c922c709e
Kemi1505 <oluwakemikuteyi1505@gmail.com>
2023-09-24 07:54:53 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Harbors code
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar('0' + num);
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
