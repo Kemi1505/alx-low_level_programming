86c5f76a24b45bdbbfedec434ef00b75f755727a
Kemi1505 <oluwakemikuteyi1505@gmail.com>
2023-09-24 06:38:08 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - not using char
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		putchar('0' + a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
