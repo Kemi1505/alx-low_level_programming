ac48665043feb997cc49f260281899a9170204e7
Kemi1505 <oluwakemikuteyi1505@gmail.com>
2023-09-25 13:51:07 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - sum of even fibonacci
+ * Return: zero
+ */
+int main(void)
+{
+	int a = 0;
+	long b = 1, c = 2, sum = c;
+
+	while ((c + b) < 4000000)
+	{
+	c += b;
+	if ((c % 2) == 0)
+	sum += c;
+	b = c - b;
+	++a;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
