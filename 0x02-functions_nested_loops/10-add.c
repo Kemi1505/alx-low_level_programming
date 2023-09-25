c9759ac991270a0f6eb89a9c524065a22da17fa0
Kemi1505 <oluwakemikuteyi1505@gmail.com>
2023-09-25 08:02:52 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Add 2 digits
+ * @a: first digit
+ * @b: second digit
+ * Return: addition
+ */
+int add(int a, int b)
+{
+	int addition = (a + b);
+
+	return (addition);
+}
