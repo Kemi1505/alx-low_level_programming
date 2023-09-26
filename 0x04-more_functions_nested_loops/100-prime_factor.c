e0524a21e0c4ecd10969ae2cf4204c7b465e19f1
Kemi1505 <oluwakemikuteyi1505@gmail.com>
2023-09-26 13:06:42 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - code
+ * Return: zero
+ */
+int main(void)
+{
+	long prime = 612852475143, divisor;
+
+	while (divisor < (prime / 2))
+	{
+		if ((prime % 2) == 0)
+		{
+			prime /= 2;
+			continue;
+		}
+		for (divisor = 3; divisor < (prime / 2); divisor += 2)
+		{
+			if ((prime % divisor) == 0)
+				prime /= divisor;
+		}
+	}
+	printf("%ld\n", prime);
+	return (0);
+}
+
