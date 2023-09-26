f0935b165115f4a26ca7ec28df68f9f60b45b47b
Kemi1505 <oluwakemikuteyi1505@gmail.com>
2023-09-26 08:45:50 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - the code
+ * @n: pointer
+ * Return: zero
+ */
+char *string_toupper(char *n)
+{
+	int a;
+
+	a = 0;
+	while (n[a] != '\0')
+	{
+		if (n[a] >= 'a' && n[a] <= 'z')
+			n[a] = n[a] - 32;
+		a++;
+	}
+	return (n);
+}
