41d1a9cc9f442a5bcc21d8cf5ba15a2f28f9e865
Kemi1505 <oluwakemikuteyi1505@gmail.com>
2023-09-26 13:31:00 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,3 +5,18 @@
  * Return: void
  */
 void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		if (a != 50)
+		{
+			if (a != 52)
+			{
+				_putchar(a);
+			}
+		}
+	}
+	_putchar('\n');
+}
