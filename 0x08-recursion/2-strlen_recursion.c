a6fc9a288b593ddaace892078d1facd78e9e3cb7
Kemi1505 <oluwakemikuteyi1505@gmail.com>
2023-10-26 21:36:41 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns length of a string
+ * @s: pointer to the string
+ * Return: void
+ */
+int _strlen_recursion(char *s)
+{
+	if (s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
