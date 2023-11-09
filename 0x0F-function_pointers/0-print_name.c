9d759b4703f0c6a1b81e3c97b172443abcbaf13f
Kemi1505 <oluwakemikuteyi1505@gmail.com>
2023-11-08 20:41:50 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - function to print name
+ * @name: the name
+ * @f: the pointer
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (i == NULL || f == NULL)
+		return;
+
+	f(name);
+}
