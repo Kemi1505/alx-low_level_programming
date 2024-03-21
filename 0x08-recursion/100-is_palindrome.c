2d4283ef1265447675de9fd8b493889409c97def
Kemi1505 <oluwakemikuteyi1505@gmail.com>
2024-03-21 06:26:13 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - string length
+ * @s: character s
+ * Return: answer one
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * compare - compare character
+ * @s: first character
+ * @n1: first number
+ * @n2: second number
+ * Return: answer
+ */
+int compare(char *s, int n1, int n2)
+{
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + compare(s, n1 + 1, n2 - 1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - check if its palindrome
+ * @s: character
+ * Return: final answer
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (compare(s, 0, _strlen_recursion(s) - 1));
+}
