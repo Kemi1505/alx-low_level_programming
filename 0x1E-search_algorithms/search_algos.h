58c8a037ed30a0d7b0b4fe20ed8344b67372473e
Kemi1505 <oluwakemikuteyi1505@gmail.com>
2024-05-08 21:29:27 -0700

    one

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,25 @@
 #ifndef search_algos.h
 #define search_algos.h
 
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif
